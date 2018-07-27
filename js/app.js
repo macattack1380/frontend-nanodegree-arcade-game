91bf85014ebd6f0fde7ff24772447e5394a42f4c
Joe McCallister <macattack1380@gmail.com>
2018-07-26 20:27:09 -0600

    Feat: Render Enemy, speed, position implemented enemy rendering, speed, placement

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y + 55; //55 used to center bug horizontally in row
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
+    this.step = 101;
+    this.boundary = this.step * 5;
+    this.resetPosition = -this.step;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +15,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x < this.boundary) {
+      this.x += this.speed * dt;
+    }
+    else{
+      this.x = this.resetPosition;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -68,7 +75,11 @@ class Hero{
 // Place the player object in a variable called player
 
 const player = new Hero();
-
+const bug1 = new Enemy(-101, 0, 200);
+const bug2 = new Enemy(-101, 83, 300);
+const bug3 = new Enemy((101*2.5), 83, 300);
+const allEnemies = [];
+allEnemies.push(bug1,bug2,bug3);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
